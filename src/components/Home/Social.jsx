7daad1d10f6102ef94f7fd36ae7a7d320cd0b4a6
MAYANK SHARMA <mayanksharma.ms.0225@gmail.com>
2024-07-19 01:24:39 +0530

    home component

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href="www.google.com" className='home__social-icon' target='_blank'><i class="uil uil-envelopes"></i></a>
+        <a href="www.google.com" className='home__social-icon' target='_blank'><i className="uil uil-linkedin"></i></a>
+        <a href="www.google.com" className='home__social-icon' target='_blank'><i className="uil uil-github"></i></a>
+    </div>
+  )
+}
+ 
+export default Social
