06f151e0472ecb6f42418b5cb60abda24ad7b87c
Aditya Dev <96416910+mank25@users.noreply.github.com>
2024-10-31 04:45:29 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/about"
 import Contact from "./components/Contact"
-import Hero from "./components/hero"
+import Hero from "./components/Hero"
 import Marquee from "./components/Marquee"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
